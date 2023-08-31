ea6850b0bbc5c366717bbf7b3643c249cbb40372
Daniel Krieg <dk.daniel.krieg@gmail.com>
2023-08-31 23:19:30 +0200

    updates

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # setup
 
-use encryptioon to push credentials to github
+clone this repo via
+`git clone https://github.com/damn-war/setup.git`
 
+set the password to encrypt as env var via
+`export pass=...`
+
+run the start.sh script via
+`./start.sh`
--- start.sh
+++ start.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-git clone https://github.com/damn-war/setup.git
-
-openssl enc -d -aes-256-cbc -in ssh.tar.gz.enc -out ssh.tar.gz
+# git clone https://github.com/damn-war/setup.git
+echo "path to your repositories"
+read
+openssl enc -d -aes-256-cbc -in ssh.tar.gz.enc -out ssh.tar.gz -k $pass
 mkdir .ssh
 tar xzvf ssh.tar.gz -C .ssh
-openssl enc -d -aes-256-cbc -in env.enc -out .env
+openssl enc -d -aes-256-cbc -in env.enc -out .env -k $oass
 
-docker run -it -v $PWD:/repositories \
+docker run -it -v $REPLY:/repositories \
   -v ./.ssh:/root/.ssh \
   -v ./.gitconfig:/root/.gitconfig \
   -v /var/run/docker.sock:/var/run/docker.sock \
